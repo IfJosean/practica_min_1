7506a98619824565b6cbb6ecb97bf51e908d13db
IfJosean <josean7jkami@gmail.com>
2017-11-12 23:47:29 +0100

    Controlador

--- src/main/java/practica_min_1/Controlador/EetakemonManager.java
+++ src/main/java/practica_min_1/Controlador/EetakemonManager.java
@@ -1,10 +1,20 @@
 package practica_min_1.Controlador;
 
+import practica_min_1.Modelo.Item;
+import practica_min_1.Modelo.User;
+
+import java.util.List;
+
 /**
  * Created by Josean on 12/11/2017.
  */
 public interface EetakemonManager {
 
-    List<User> getAll()
-        boolean set(User u)
+        List<User> getAll();
+        boolean set(User u);
+        boolean edit(User u);
+        User get(int id);
+        List<Item> getItems(int userId);
+        boolean setItem(int userId; Item i);
+
 }
